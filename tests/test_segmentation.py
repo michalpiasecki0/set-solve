e5c174b0239d22b1372902d2bc96865b72a8dbbf
Misha Piasecki <michal.piasecki0@gmail.com>
2025-03-16 14:51:37 +0100

    segmentation

---
+++ tests/test_segmentation.py
@@ -0,0 +1,18 @@
+import sys
+import cv2
+from pathlib import Path
+
+# add module root path
+root_path = str(Path(".").resolve())
+sys.path.append(str(Path(".").resolve()))
+
+from src.segmentation import detect_cards
+
+def test_detection():
+    # fix to do -> incorrect for 14 cards
+    paths = [f"tests/data/{i}.jpg" for i in [0, 8]] # gt = [12, 12, 14]
+    imgs = [cv2.imread(path) for path in paths]
+    detected_cards = list(map(len, [detect_cards(img) for img in imgs]))
+    
+    assert detected_cards == [12, 12]
+    
