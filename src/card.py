064c79d3de6b58da84df062f32d912399739e071
Misha Piasecki <michal.piasecki0@gmail.com>
2025-03-23 19:46:56 +0100

    remove card

--- src/card.py
+++
@@ -1,9 +0,0 @@
-class DetectedCard:
-    def __init__(self, img_bgr, rect):
-        self.img_bgr = img_bgr
-        self.rect = rect
-
-    @property
-    def color(self):
-        color = self.img_bgr.mean(axis=(0, 1))
-        return color
