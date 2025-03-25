9c04e6566ae9d19ff2ee7c2b33580d8359b29343
Misha Piasecki <michal.piasecki0@gmail.com>
2025-03-25 12:57:55 +0100

    change mappings to be cmpatible with pytorch crossentropy

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,4 @@
-COLOR_MAPPING = {"Red": 1, "Green": 2, "Purple": 3}
-FILLMENT_MAPPING = {"Empty": 1, "Half": 2, "Full": 3}
-SHAPE_MAPPING = {"Diamond": 1, "Oval": 2, "Squiggle": 3}
+COLOR_MAPPING = {"Red": 0, "Green": 1, "Purple": 2}
+FILLMENT_MAPPING = {"Empty": 0, "Half": 1, "Full": 2}
+SHAPE_MAPPING = {"Diamond": 0, "Oval": 1, "Squiggle": 2}
+COUNT_MAPPING = {1: 0, 2: 1, 3: 2}
